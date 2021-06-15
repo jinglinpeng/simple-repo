528bfa14c3f527c7e5eb17ebed1074448aeea3dd
github-action-benchmark <github@users.noreply.github.com>
2021-06-15 18:33:44 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 21d151da1f906637fa0b8d9a08dc9d93b90e25d9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1623781847274,
+  "lastUpdate": 1623782024648,
   "repoUrl": "https://github.com/jinglinpeng/simple-repo",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -38,6 +38,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00019913105419657232",
             "extra": "mean: 2.715151876315895 msec\nrounds: 380"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jlpengcs@gmail.com",
+            "name": "jinglinpeng",
+            "username": "jinglinpeng"
+          },
+          "committer": {
+            "email": "jlpengcs@gmail.com",
+            "name": "jinglinpeng",
+            "username": "jinglinpeng"
+          },
+          "distinct": true,
+          "id": "21d151da1f906637fa0b8d9a08dc9d93b90e25d9",
+          "message": "a",
+          "timestamp": "2021-06-15T11:32:49-07:00",
+          "tree_id": "95cbd73c7ffd8ed22e6801e79dcd2c31757816e5",
+          "url": "https://github.com/jinglinpeng/simple-repo/commit/21d151da1f906637fa0b8d9a08dc9d93b90e25d9"
+        },
+        "date": 1623782024152,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "bench.py::test_fib_10",
+            "value": 0.49947783268749496,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00002549598734042957",
+            "extra": "mean: 2.002090852799995 sec\nrounds: 5"
+          },
+          {
+            "name": "bench.py::test_fib_20",
+            "value": 0.4994741667833299,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00003932008113400075",
+            "extra": "mean: 2.0021055471999945 sec\nrounds: 5"
           }
         ]
       }
